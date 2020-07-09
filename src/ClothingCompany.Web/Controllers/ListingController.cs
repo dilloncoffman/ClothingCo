ea70212d1d8d909ade2ab794d1c452fd04e5b092
Dillon Coffman <dcoffman@ceiamerica.com>
2020-07-09 16:23:08 -0400

    Created Listing model, controller, and view to render out a list of articles by fetching the articles from the Sitecore Content Tree by their data source item ids and then rendering them

---
+++ src/ClothingCompany.Web/Controllers/ListingController.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using ClothingCompany.Models;
+using Sitecore.Links;
+using Sitecore.Mvc.Presentation;
+
+namespace ClothingCompany.Controllers
+{
+    public class ListingController : Controller
+    {
+        // GET: Listing
+        public ActionResult Index()
+        {
+            var dataSourceId = RenderingContext.CurrentOrNull.Rendering.DataSource;
+            var pageList = new Listing();
+
+            if (dataSourceId != null)
+            {
+                var dataSource = Sitecore.Context.Database.GetItem(dataSourceId);
+                if (dataSource != null)
+                {
+                    var listofArticles = dataSource.Children;
+                    pageList.Posts = listofArticles.Select(item => new NavItem
+                    {
+                        Item = item,
+                        Url = LinkManager.GetItemUrl(item),
+                    }).ToList();
+                }
+            }
+            return View(pageList);
+        }
+    }
+}
---
+++ src/ClothingCompany.Web/Models/Listing.cs
@@ -0,0 +1,12 @@
+﻿using Sitecore.Data.Items;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ClothingCompany.Models
+{
+    public class Listing
+    {
+        public Item ParentItem { get; set; }
+        public IEnumerable<NavItem> Posts { get; set; } = Enumerable.Empty<NavItem>();
+    }
+}
